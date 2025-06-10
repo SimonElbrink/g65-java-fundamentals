1615f275a27a3dfa3b6e627473a60bef285be577
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2025-06-10 09:56:09 +0200

    Covered Comments

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,9 +1,30 @@
 package se.lexicon;
 
+
+// Special type of comment used to generate documentation.
+/**
+ * Hello World
+ */
 public class Main {
 
+    // This is a single line comment
+
+    /* This is a regular multi-line comment
+    * It spans multiple lines
+    */
+
+    /**
+     * This is a Java Doc ( Documentation Comments ).
+     * Used to create documentation for Java code.
+     * These comments are processed by the Javadoc tool to generate HTML documentation for your program.
+     * https://www.baeldung.com/javadoc
+     */
+
+
     public static void main(String[] args) {
+
         System.out.println("Hello Group 56");
+
     }
 
 }
