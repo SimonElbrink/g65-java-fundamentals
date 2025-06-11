e56ff4098ba8b408dc4f9d2b08bd6fcb80a3dafd
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2025-06-11 10:47:30 +0200

    Demo1 starting point

---
+++ src/main/java/se/lexicon/Demo.java
@@ -0,0 +1,19 @@
+package se.lexicon;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        int num1 = 5, num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+
+        num1 = 2;
+        num2 = 5;
+        sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+
+
+
+    }
+}
